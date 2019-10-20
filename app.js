cffa39d6bdf06ddfa767f02998f0e1c664fd3889
Amaury ADJA <primal.coding@gmail.com>
2019-10-20 14:19:48 +0100

    3 done, on the way to 4

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-const fs = require('fs');
+const notes = require('./notes.js')
 
-fs.writeFileSync('notes.txt', 'This file was created by node.js!');
+const text = notes()
+
+console.log(notes())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const util = require('./util.js')
+
+// // const name = "MyName"
+
+// // const sum = util (4, -2)
+
+// console.log(util)
+
+
+
+
+
+
+//const fs = require('fs')
+
+//fs.writeFileSync('notes.txt','Hey Sure!')
+//fs.appendFileSync('notes.txt', '\nLet\'s add: some more something')
---
+++ util.js
@@ -0,0 +1,10 @@
+console.log('util.js')
+
+const name = ('Jacko')
+
+const add = function (a, b) {
+    return a + b
+}
+
+// module.exports = add
+module.exports = name
